284f567328dfe00e41041b14e628c5cf8c7aeb22
rushikeshswakchaure <89862923+rushikeshswakchaure@users.noreply.github.com>
2023-09-19 14:46:00 +0530

    addition of two numbers

---
+++ Addition.java
@@ -0,0 +1,12 @@
+class Addition
+{
+	public static void main(String[] args) 
+	{
+      int a=10;
+      int b=20;
+      int c=30;
+      int d=40;
+     int Result=a+b+c+d;
+		System.out.println("Addition of four numbers"+Result);
+	}
+}
