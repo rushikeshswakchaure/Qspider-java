7421f611de3d88880157e480cd9fff6ba9cfffd5
rushikeshswakchaure <89862923+rushikeshswakchaure@users.noreply.github.com>
2023-09-19 15:24:43 +0530

    find area of reactangle

---
+++ Reactangle.java
@@ -0,0 +1,10 @@
+class Reactangle 
+{
+	public static void main(String[] args) 
+	{
+      int  l=12;
+      int  b=16;
+      int Result=l*b;
+	  System.out.println("Area of reactangle is:"+Result);
+	}
+}
